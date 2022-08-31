174b548a13024765a4b394c6118f0ae1cf461ac6
Zatoalone <eremin-aleksandr@inbox.ru>
2022-08-31 15:41:30 +0400

    Добавлены отсутствующие зависимости

--- config/settings.py
+++ config/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%*)0kniah=!8&sq6cij6h=m_t^k3th0i92!lxoajed6$vz62bi'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
