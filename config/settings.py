7e411cc91fa71efde7182bb041e5a86d926429f0
Zatoalone <eremin-aleksandr@inbox.ru>
2022-08-31 15:26:33 +0400

    Перенесено меню пользователя

--- config/settings.py
+++ config/settings.py
@@ -117,11 +117,9 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
-#LOGIN_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = 'login'
 
-
-
-#LOGOUT_REDIRECT_URL = 'login'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
